2081f9f22cf476442b6e0711f1da132d90b5936b
esoft34 - Mateus G. Geracino <mateusgoncalvesgeracino@hotmail.com>
2020-10-16 22:46:16 -0300

    add id offer

--- src/backend/microservices/backoffice/6MKT.BackOffice.Api/Models/Responses/Offers/OfferResponse.cs
+++ src/backend/microservices/backoffice/6MKT.BackOffice.Api/Models/Responses/Offers/OfferResponse.cs
@@ -4,6 +4,7 @@ namespace _6MKT.BackOffice.Api.Models.Responses.Offers
 {
     public class OfferResponse
     {
+        public long Id { get; set; }
         public string Description { get; set; }
         public bool InStock { get; set; }
         public double Price { get; set; }
